b2fc11a7a2dede8bb63103a6ec1a9f512335b5a6
noschiff <noah@wow.com>
2019-02-17 22:11:45 -0500

    updated testing code

--- src/com/noschiff/Main.java
+++ src/com/noschiff/Main.java
@@ -1,9 +1,5 @@
 package com.noschiff;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
-
 /**
  * Driver class to test the trees
  *
@@ -12,20 +8,25 @@ import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) {
-/*
+
         //hard code using lambda (InitTree interface)
         Tree lambdaTree = new Tree(() -> {
-            Node root = new Node(Operator.add);
-            root.leftChild = new Node(Operator.exp);
-            root.leftChild.leftChild = new Node(5);
-            root.leftChild.rightChild = new Node(2);
-            root.rightChild = new Node(Operator.mult);
-            root.rightChild.rightChild = new Node(2);
-            root.rightChild.leftChild = new Node(5);
+            Node root = new Node(new Operation(Operator.add));
+
+            root.leftChild = new Node(new Operation(Operator.sub));
+            root.leftChild.leftChild = new Node(new Operation(Operator.exp));
+            root.leftChild.leftChild.leftChild = new Node(new Number(9.0));
+            root.leftChild.leftChild.rightChild = new Node(new Number(0.5));
+            root.leftChild.rightChild = new Node(new Number(2.0));
+
+            root.rightChild = new Node(new Operation(Operator.mult));
+            root.rightChild.leftChild = new Node(new Number(5.0));
+            root.rightChild.rightChild = new Node(new Number(4.0));
+
             return root;
         });
         System.out.println(lambdaTree);
-*/
+
         //build using postfix string
         Tree post = new Tree("9.0 0.5 ^ 2.0 - 5.0 4.0 × +", Form.postfix);
         Tree pre = new Tree("+ - ^ 9.0 0.5 2.0 × 5.0 4.0", Form.prefix);
@@ -39,7 +40,7 @@ public class Main {
         System.out.println(elementArr);
 
         //checks if the trees are equal
-        System.out.println(post.equals(pre) && pre.equals(infix) && infix.equals(elementArr));
+        System.out.println(post.equals(pre) && pre.equals(infix) && infix.equals(elementArr) && elementArr.equals(lambdaTree));
     }
 
     private static String convert(String expression, Form from, Form to) {
